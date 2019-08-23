e03e0c21bccc54475ef62d769b1061b4289a8760
garinscater <53307606+garinscater@users.noreply.github.com>
2019-08-23 17:09:00 +0700

    Tutorial.php

---
+++ Tutorial.php
@@ -0,0 +1,19 @@
+# cal
+_   _    _    ____ _  _______ ____
+| | | |  / \  / ___| |/ / ____|  _ \
+| |_| | / _ \| |   | ' /|  _| | |_) |
+|  _  |/ ___ \ |___| . \| |___|  _ <
+|_| |_/_/   \_\____|_|\_\_____|_| \_\
+
++-------------------------------+
+| author:Mr.G4R1N               |
+| FB :G4R1N SC4T3R              |
+| YT:Hacker Elite ID            |
+| Github:github.com/garinscater |
++-------------------------------+
+Pkg install nano
+Cd ..
+Cd usr/etc
+Nano bash.bashrc
+Ketik cal | lolcat
+Selesai
